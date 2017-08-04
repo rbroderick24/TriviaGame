a6f4e1af198fb40b3ff61a7f619f231ad0f8efdf
rbroderick24 <30726596+rbroderick24@users.noreply.github.com>
2017-08-04 11:59:24 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(522, 256)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(60, 23)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Shen"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(522, 279)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 23)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Yone"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(522, 302)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(67, 23)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Riven"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton4.Location = New System.Drawing.Point(522, 325)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(72, 23)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Darius"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(522, 348)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 33)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.Label1.Location = New System.Drawing.Point(491, 221)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(390, 28)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "What is the Name of Yasuo's Brother?"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(22, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Q1"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(22, 41)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Q2"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(22, 70)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Q3"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(22, 99)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Q4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(22, 128)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "Q5"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(22, 157)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "Q6"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(22, 186)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 12
+        Me.Button8.Text = "Q7"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(22, 215)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 13
+        Me.Button9.Text = "Q8"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(22, 244)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 14
+        Me.Button10.Text = "Q9"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(22, 273)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 15
+        Me.Button11.Text = "Q10"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(536, 414)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(998, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LimeGreen
+        Me.ClientSize = New System.Drawing.Size(1115, 612)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.ForeColor = System.Drawing.Color.DarkBlue
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1131, 651)
+        Me.MinimumSize = New System.Drawing.Size(1131, 651)
+        Me.Name = "Form1"
+        Me.Text = "Trivia Game"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,175 @@
+﻿Public Class Form1
+    Dim Question As Integer
+    Dim Score As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label3.Text = "Score: 0"
+        Label1.Text = "League Of Legends Trivia Game"
+        RadioButton1.Text = "Answer 1"
+        RadioButton2.Text = "Answer 2"
+        RadioButton3.Text = "Answer 3"
+        RadioButton4.Text = "Answer 4"
+        Label2.Text = "Select a Question to Begin"
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Question = 1
+        Label1.Text = "What is the name of Yasuo's Brother?"
+        RadioButton1.Text = "Shen"
+        RadioButton2.Text = "Yone"
+        RadioButton3.Text = "Riven"
+        RadioButton4.Text = "Darius"
+        Label2.Text = "Question 1"
+        Button2.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Question = 2
+        Label1.Text = "Where in Runeterra is Riven from?"
+        RadioButton1.Text = "Zaun"
+        RadioButton2.Text = "Piltover"
+        RadioButton3.Text = "Demacia"
+        RadioButton4.Text = "Noxus"
+        Label2.Text = "Question 2"
+        Button3.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Question = 3
+        Label1.Text = "What is the Name of Jax's Ultimate?"
+        RadioButton1.Text = "Counter Strike"
+        RadioButton2.Text = "Leap Strike"
+        RadioButton3.Text = "Grandmaster's Might"
+        RadioButton4.Text = "Empower"
+        Label2.Text = "Question 3"
+        Button4.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Question = 4
+        Label1.Text = "What is the Name of Kalista's Passive?"
+        RadioButton1.Text = "Rend"
+        RadioButton2.Text = "Martial Poise"
+        RadioButton3.Text = "Spear of Vengeance"
+        RadioButton4.Text = "Fate's Call"
+        Label2.Text = "Question 4"
+        Button5.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Question = 5
+        Label1.Text = "What is Caitlyn's Title?"
+        RadioButton1.Text = "The Sheriff of Piltover"
+        RadioButton2.Text = "The Hexplosives Expert"
+        RadioButton3.Text = "The Bounty Hunter"
+        RadioButton4.Text = "The Sherff of Zaun"
+        Label2.Text = "Question 5"
+        Button6.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Question = 6
+        Label1.Text = "What is the Name of Seguani's Mount?"
+        RadioButton1.Text = "Zed"
+        RadioButton2.Text = "Trundle"
+        RadioButton3.Text = "Bristle"
+        RadioButton4.Text = "Lissandra"
+        Label2.Text = "Question 6"
+        Button7.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Question = 7
+        Label1.Text = "Who helps Shen Maintain Balance throughout Runeterra?"
+        RadioButton1.Text = "Akali and Kennen"
+        RadioButton2.Text = "Kennen and Zed"
+        RadioButton3.Text = "Akali and Lee Sin"
+        RadioButton4.Text = "Akali and Zed"
+        Label2.Text = "Question 7"
+        Button8.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Question = 8
+        Label1.Text = "What Jungle Camp Gives you Mana/Energy Regen and Cooldown Reduction?"
+        RadioButton1.Text = "Red Buff"
+        RadioButton2.Text = "Wolf Camp"
+        RadioButton3.Text = "Raptor Camp"
+        RadioButton4.Text = "Blue Buff"
+        Label2.Text = "Question 8"
+        Button9.Enabled = False
+        Button1.Enabled = True
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Question = 9
+        Label1.Text = "What Lane Normally has Assassins and Mages?"
+        RadioButton1.Text = "Top Lane"
+        RadioButton2.Text = "Jungle"
+        RadioButton3.Text = "Mid Lane"
+        RadioButton4.Text = "Bottom Lane"
+        Label2.Text = "Question 9"
+        Button10.Enabled = False
+        Button1.Enabled = True
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Question = 10
+        Label1.Text = "What Company Created League of Legends?"
+        RadioButton1.Text = "Blizzard"
+        RadioButton2.Text = "Riot Games"
+        RadioButton3.Text = "Treyarch"
+        RadioButton4.Text = "Mojang"
+        Label2.Text = "Question 10"
+        Button11.Enabled = False
+        Button1.Enabled = True
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton2.Checked And Question = 1 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton4.Checked And Question = 2 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton3.Checked And Question = 3 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton2.Checked And Question = 4 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton1.Checked And Question = 5 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton3.Checked And Question = 6 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton1.Checked And Question = 7 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton4.Checked And Question = 8 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton3.Checked And Question = 9 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        ElseIf RadioButton2.Checked And Question = 10 Then
+            MsgBox("Correct")
+            Score = Score + 1
+            Button1.Enabled = False
+        Else MsgBox("Incorrect")
+            Button1.Enabled = False
+        End If
+        Label3.Text = "Score:" & Score
+        If Question = 10 And Button1.Enabled = False Then
+            MsgBox("Congrats! You Finished the Trivia Game!")
+        End If
+    End Sub
+
+End Class
